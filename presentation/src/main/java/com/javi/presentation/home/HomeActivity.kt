ed82a9f2ac81614bd22e60811bc795d48dcc8d4c
Javi Caselles <casellesjavi@gmail.com>
2023-10-21 13:52:52 +0200

    implemented basic bottom navigation component

--- presentation/src/main/java/com/javi/presentation/home/HomeActivity.kt
+++ presentation/src/main/java/com/javi/presentation/home/HomeActivity.kt
@@ -2,11 +2,14 @@ package com.javi.presentation.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.javi.presentation.R
+import com.javi.presentation.components.HomeBottomNavigation
 import com.javi.presentation.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), HomeBottomNavigation {
 
     private lateinit var binding: ActivityHomeBinding
 
@@ -14,5 +17,19 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.homeBottomNav.setListeners(this)
+    }
+
+    override fun onFavouritesClick() {
+        binding.navHostFragment.findNavController().navigate(R.id.home_favourite_books)
+    }
+
+    override fun onAllBooksClick() {
+        binding.navHostFragment.findNavController().navigate(R.id.home_all_books)
+    }
+
+    override fun onAllUsersClick() {
+        binding.navHostFragment.findNavController().navigate(R.id.home_all_users)
     }
 }
