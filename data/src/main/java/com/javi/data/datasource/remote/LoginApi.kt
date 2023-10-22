b2f832a1b864226b22c0b1f294317ff6db58839a
Javi Caselles <casellesjavi@gmail.com>
2023-10-22 18:12:52 +0200

    removed not used ui state model

--- data/src/main/java/com/javi/data/datasource/remote/LoginApi.kt
+++ data/src/main/java/com/javi/data/datasource/remote/LoginApi.kt
@@ -5,5 +5,5 @@ import com.javi.data.dto.UserDto
 interface LoginApi {
     fun doLogin(username: String, password: String): UserDto
     fun doLoginWithToken(token: String): UserDto
-    fun doLogout(): Unit
+    fun doLogout()
 }
--- presentation/src/main/java/com/javi/presentation/model/UiState.kt
+++
@@ -1,7 +0,0 @@
-package com.javi.presentation.model
-
-sealed class UiState {
-    object Loading: UiState()
-    data class Success<T>(val data: T): UiState()
-    data class Error(private val error: String): UiState()
-}
