a2b94ddfea852bbcffca4a6fbe97b996f052b488
Javi Caselles <casellesjavi@gmail.com>
2023-11-09 14:53:53 +0100

    added versions catalogs to buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-    implementation("com.android.tools.build:gradle:8.1.2")
+    implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.gradle.plugin)
 }
 
 val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
